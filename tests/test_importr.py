22049c4a82c1005d1d54814c0a67152fbb444ea3
Kelvin <kt16@sanger.ac.uk>
2021-06-16 10:25:28 +0100

    Update test_importr.py

--- tests/test_importr.py
+++ tests/test_importr.py
@@ -3,6 +3,6 @@ import rpy2
 from rpy2.robjects.packages import importr
 
 
-def test_importrpy2():    
-    sh = importr('shazam')
-    assert sh.__module__ == 'rpy2.robjects.packages'
+def test_importrpy2():
+    base = importr('base')
+    assert base.__module__ == 'rpy2.robjects.packages'
