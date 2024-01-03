8a154e7e23ef8a2b45c0e36fdd4cd4aa5315fa66
Zewen Kelvin Tuong <z.tuong@uq.edu.au>
2024-01-03 17:52:29 +1000

    change repo

--- tests/test_importr.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import rpy2
-from rpy2.robjects.packages import importr
-
-
-def test_importrpy2():
-    pkg = importr('remotes')
-    assert pkg.__module__ == 'rpy2.robjects.packages'
